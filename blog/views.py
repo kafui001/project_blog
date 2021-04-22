91db1687766711b5e4b2c28aa3c5b66faa111738
kafui001 <kafui01@yahoo.com>
2021-04-22 01:57:36 -0400

    refactored footer.html, added request.post functionality to blog_home.html

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView, CreateView
 
 
 from .models import Post, Category
 from .forms import PostForm
+from core.forms import ContactForm
+
 # Create your views here.
 # def bloghome(request):
 #     return render(request, 'blog/blog_home.html')
@@ -12,8 +15,25 @@ class BlogHomeView(ListView):
     model = Post
     template_name = 'blog/blog_home.html'
     ordering = ['-date_created']
+    paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogHomeView, self).get_context_data(**kwargs)
+        context['category'] = Category.objects.all()
+        context['form'] = ContactForm
+        return context 
 
 class BlogAddView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/blog_create.html'
+
+
+    def get_success_url(self):
+        return reverse('blog_home')
+
+    def form_valid(self, form):
+        form = form.save(commit=False)
+        form.author = self.request.user
+        form.save()
+        return super(BlogAddView, self).form_valid(form)
